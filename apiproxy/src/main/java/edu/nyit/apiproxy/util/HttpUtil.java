7bf8596ef60bf10476a7282633c5c573237bb373
Tao Wang <wtforfun540@gmail.com>
2022-11-08 10:41:30 -0800

    1, test commit after setting to a private repository

--- apiproxy/src/main/java/edu/nyit/apiproxy/util/HttpUtil.java
+++ apiproxy/src/main/java/edu/nyit/apiproxy/util/HttpUtil.java
@@ -1,12 +1,15 @@
 package edu.nyit.apiproxy.util;
 
+
 /**
  * @author wangtao
  * @date 2022/11/3 18:05
  */
 public class HttpUtil {
+    
 
     public static Object httpGet(){
+        // TODO: 2022/11/8 待实现 Ying
         return null;
     }
 
